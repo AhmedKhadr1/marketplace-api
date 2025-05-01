a1629aaf52a7d4fa6debeb67fbc8c29fe907b479
Ahmed Khadr <https://github.com/AhmedKhadr1>
2025-05-01 06:56:39 +0300

    Add package.json and stdart sscript

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.static('.'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'show_result.html'));
 });
+
 // Debug: Log ENV info
 console.log("✅ Application started");
 
@@ -266,6 +267,7 @@ app.get('/debug/all-tables', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('✅ Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
 });
